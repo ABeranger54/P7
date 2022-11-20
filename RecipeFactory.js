0d15335e808bc53512b1805c8a0164d75233ad72
Alex2204 <Alex2204@ALEX2204-PC>
2022-11-20 20:57:49 +0100

    - Ajout d'un tri par bar de recherche - Ajout des champs de recherche avancés en HTML

--- RecipeFactory.js
+++ RecipeFactory.js
@@ -7,4 +7,34 @@ class RecipeFactory{
         RecipeFactory.RECIPES = new Array();
         recipesList.forEach(r => RecipeFactory.RECIPES.push(new Recipe(r)));
     }
+
+    static filterFromBar1(input){
+        const res = RecipeFactory.RECIPES.filter(function(r){
+            const inName = r._name.toLowerCase().includes(input.toLowerCase());
+            const inDescription = r._description.toLowerCase().includes(input.toLowerCase());
+            const inIngredients = r._ingredients.filter(i => i.ingredient.toLowerCase().includes(input.toLowerCase()));
+            
+           return inName || inDescription || inIngredients.length > 0;
+        });
+        return res;
+    }
+
+    static filterFromBar2(input){
+        var res = [];
+        RecipeFactory.RECIPES.forEach(function(r){
+            var isValid = false;
+            if(r._name.toLowerCase().includes(input.toLowerCase()) || r._description.toLowerCase().includes(input.toLowerCase())){
+                isValid = true;
+            }
+            r._ingredients.forEach(function(i){
+                if(i.ingredient.toLowerCase().includes(input.toLowerCase())){
+                    isValid = true;
+                }
+            });
+            if(isValid){
+                res.push(r);
+            }
+        });
+        return res;
+    }
 }
--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function init(){
     RecipeFactory.init();
     displayRecipes(RecipeFactory.RECIPES);
+
+    document.getElementById("searchBar").addEventListener("keyup", function(){
+        displayRecipes(getResultFromBar());
+    });
+
+    document.querySelector("#ingredientList .cover").addEventListener("click", openIngredients);
+    document.querySelector("#ingredientList .search img").addEventListener("click", closeIngredients);
 }
 
 function displayRecipes(recipes){
     const main = document.querySelector("main");
+    main.innerHTML = "";
     recipes.forEach(r => main.appendChild(r.getCardDOM()));
+}
+
+function getResultFromBar(){
+    const sbValue = document.getElementById("searchBar").value;
+    if(sbValue.length < 3){
+        return RecipeFactory.RECIPES;
+    }
+    return RecipeFactory.filterFromBar2(sbValue);
+}
+
+function openIngredients(){
+    const igList = document.querySelector("#ingredientList");
+    igList.querySelector(".cover").style.display = "none";
+    igList.querySelector(".content").style.display = "block";
+}
+
+function closeIngredients(){
+    const igList = document.querySelector("#ingredientList");
+    igList.querySelector(".cover").style.display = "flex";
+    igList.querySelector(".content").style.display = "none";
 }
 
 init();
